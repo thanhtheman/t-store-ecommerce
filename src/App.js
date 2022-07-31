b7594ba872b866dc655e39ec70b3ed8d5e4626d0
thanh quach <thanhb.quach@gmail.com>
2022-07-30 21:36:50 -0400

    restyle navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import ThemProvider from 'react-bootstrap/ThemeProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
+    <ThemProvider
+    breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+    minBreakpoint="xxs">
     <div>
       <Navbar />
       <Hero />
     </div>
+    </ThemProvider>
     
   )
 }
